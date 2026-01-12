c0581809f3f861eb999cc2fb45aeceabfedfd115
yngvildu <szucka.marzena@gmail.com>
2026-01-12 16:31:58 +0100

    Basic tests for Amazon website

---
+++ Test Automation/Amazon/Search_menu.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from Open_browser import *
+
+def search_menu():
+    driver = get_driver()
+    url = 'https://www.amazon.pl/'
+
+    try: 
+        driver.get(url)
+        accept_cookies(driver)
+        wait = WebDriverWait(driver, 10)
+        menu_btn = wait.until(EC.presence_of_element_located((By.ID, "nav-hamburger-menu")))
+        menu_btn.click()
+        category_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[text()='Książki']")))
+        category_btn.click()
+        polishbooks_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.hmenu-item[href*='polish']")))       
+        driver.execute_script("arguments[0].click();", polishbooks_btn)
+        assert "polish" in driver.current_url
+        print("Search submitted successfully.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    finally:
+        driver.quit()
+        print("Browser closed.")
+
+if __name__ == "__main__":
+    search_menu() 
