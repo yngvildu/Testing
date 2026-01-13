9605b093fe7a04bdb950885fcbef950465b5e7ed
Marzena <91874472+yngvildu@users.noreply.github.com>
2026-01-13 10:51:07 +0100

    Update test_search_text.py

--- Test Automation/Amazon/test_search_text.py
+++ Test Automation/Amazon/test_search_text.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys  
-from Open_browser import * 
+from test_open_browser import *
 
 def test_search():
     driver = test_get_driver()
