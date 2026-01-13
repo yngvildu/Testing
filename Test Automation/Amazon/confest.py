eab5bd0fe577cb176c989dc2cfa86c1838b0d7db
Marzena <91874472+yngvildu@users.noreply.github.com>
2026-01-13 12:44:33 +0100

    Update confest.py

--- Test Automation/Amazon/confest.py
+++ Test Automation/Amazon/confest.py
@@ -1,15 +1,20 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless') 
+    options = Options()
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    yield driver 
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--disable-gpu')
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     
-    driver.quit() 
+    yield driver
+    
+    driver.quit()
