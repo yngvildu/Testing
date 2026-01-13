d385c5f331e5319fe3c48386bd1dc12654bcd5c6
Marzena <91874472+yngvildu@users.noreply.github.com>
2026-01-13 12:53:36 +0100

    Update test_search_menu.py

--- Test Automation/Amazon - jenkins/test_search_menu.py
+++ Test Automation/Amazon - jenkins/test_search_menu.py
@@ -5,29 +5,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from test_open_browser import *
 
-def test_search_menu(test_get_driver):
-    driver = test_get_driver 
+def test_search_menu(driver):
     url = 'https://www.amazon.pl/'
-
-    try: 
-        driver.get(url)
-        test_accept_cookies(driver)
-        wait = WebDriverWait(driver, 10)
-        menu_btn = wait.until(EC.presence_of_element_located((By.ID, "nav-hamburger-menu")))
-        menu_btn.click()
-        category_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[text()='Książki']")))
-        category_btn.click()
-        polishbooks_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.hmenu-item[href*='polish']")))       
-        driver.execute_script("arguments[0].click();", polishbooks_btn)
-        assert "polish" in driver.current_url
-        print("Search submitted successfully.")
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-    
-    finally:
-        driver.quit()
-        print("Browser closed.")
-
-if __name__ == "__main__":
-    test_search_menu() 
+    driver.get(url)
+    accept_cookies(driver)
+    wait = WebDriverWait(driver, 10)
+    menu_btn = wait.until(EC.element_to_be_clickable((By.ID, "nav-hamburger-menu")))
+    menu_btn.click()
+    category_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[text()='Książki']")))
+    category_btn.click()
+    polishbooks_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.hmenu-item[href*='polish']")))       
+    driver.execute_script("arguments[0].click();", polishbooks_btn)
+    assert "polish" in driver.current_url.lower()
+    print("Test passed: Polish books section reached.")
