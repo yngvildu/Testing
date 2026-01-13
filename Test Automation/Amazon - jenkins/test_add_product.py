dd592727923a77d4f5321c0f04a9486c1edda07f
Marzena <91874472+yngvildu@users.noreply.github.com>
2026-01-13 13:31:00 +0100

    Update test_add_product.py

--- Test Automation/Amazon - jenkins/test_add_product.py
+++ Test Automation/Amazon - jenkins/test_add_product.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from test_open_browser import *
+from test_open_browser import accept_cookies
 import time
 
 def test_add_product(driver):
