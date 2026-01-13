644c70fcd06b70ec88ba57231b1943d6adeedf4a
Marzena <91874472+yngvildu@users.noreply.github.com>
2026-01-13 11:08:09 +0100

    Update test_open_browser.py

--- Test Automation/Amazon/test_open_browser.py
+++ Test Automation/Amazon/test_open_browser.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.fixture
 def test_get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
