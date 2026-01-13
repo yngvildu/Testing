7428d7316fad8ced70e5bdcecb0b109b4b23a4e1
Marzena <91874472+yngvildu@users.noreply.github.com>
2026-01-13 13:31:19 +0100

    Update test_search_text.py

--- Test Automation/Amazon - jenkins/test_search_text.py
+++ Test Automation/Amazon - jenkins/test_search_text.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys  
-from test_open_browser import *
+from test_open_browser import accept_cookies
 
 def test_search(driver):
     url = 'https://www.amazon.pl/'
